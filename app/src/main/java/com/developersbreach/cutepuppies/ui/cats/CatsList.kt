049d917d6367c6f45f168e70cc5fbf0581490697
Raj <rajashekarraju123@gmail.com>
2022-07-31 01:23:13 +0530

    Removed deprecated API's, upgraded project dependencies and build tools

--- app/src/main/java/com/developersbreach/cutepuppies/ui/cats/CatsList.kt
+++ app/src/main/java/com/developersbreach/cutepuppies/ui/cats/CatsList.kt
@@ -1,35 +1,16 @@
 package com.developersbreach.cutepuppies.ui.cats
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -72,15 +53,16 @@ fun CatsList(
                 )
             }
         }
-    ) {
-        CatsListBody(selectedCat)
+    ) { paddingValues ->
+        CatsListBody(selectedCat, paddingValues)
     }
 }
 
 // Start of List
 @Composable
 private fun CatsListBody(
-    selectedCat: (Int) -> Unit
+    selectedCat: (Int) -> Unit,
+    paddingValues: PaddingValues
 ) {
     // Save the scroll state of cats list
     val scrollState = rememberLazyListState()
@@ -91,6 +73,7 @@ private fun CatsListBody(
         horizontalArrangement = Arrangement.spacedBy(20.dp),
         modifier = Modifier
             .padding(16.dp)
+            .padding(paddingValues)
             .fillMaxSize(),
         state = scrollState,
         verticalAlignment = Alignment.CenterVertically
@@ -102,7 +85,6 @@ private fun CatsListBody(
 }
 
 // Item row for one cat.
-@OptIn(ExperimentalAnimationApi::class)
 @Composable
 private fun CatRow(
     cat: Cats,
